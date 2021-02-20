734cc1771f27a5c59c06a8da5c3a1176a8ec2c80
fredmessias43 <fredmessias43@gmail.com>
2021-02-19 23:37:01 -0300

    aula 21, 22 - add route params and add jsx-one-expression-per-line: off

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepoParams {
+  repo: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepoParams>();
+  return (
+    <>
+      <h1> Repository</h1>
+      <p>{params.repo}</p>
+    </>
+  );
 };
 
 export default Repository;
