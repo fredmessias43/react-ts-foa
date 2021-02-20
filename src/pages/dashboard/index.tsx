7dfd7afb6a2c4101af96afc4541637790a158b3b
fredmessias43 <fredmessias43@gmail.com>
2021-02-19 23:07:26 -0300

    aula 18,19 - trating error and css

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,40 +1,52 @@
+/* eslint-disable camelcase */
 import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 // import Logo from '../../assets/logo.svg';
 
 import api from '../../services/api';
-import Repository from '../repository';
-import repository from '../repository';
 
 interface IRepository {
-  fullName: string;
+  full_name: string;
   description: string;
   owner: {
     login: string;
-    avatarUrl: string;
+    avatar_url: string;
   };
 }
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [error, setError] = useState('');
   const [repositories, setRepositories] = useState<IRepository[]>([]);
 
   async function handleAddRepository(
     e: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     e.preventDefault();
-    const res = await api.get<IRepository>(`/repos/${newRepo}`);
-    const repository = res.data;
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+    if (!newRepo) {
+      setError('Preencha algo');
+      setNewRepo('');
+      return;
+    }
+    try {
+      const res = await api.get<IRepository>(`/repos/${newRepo}`);
+      const repository = res.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setError('');
+    } catch (err) {
+      setError(`${err.response.status} - eRRO NA BUCA DO REPO`);
+      setNewRepo('');
+    }
   }
 
   return (
     <>
       {/* <img src={Logo} alt="Github Explorer" /> */}
       <Title>Explorer Repositories in GitHub</Title>
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!error} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={e => setNewRepo(e.target.value)}
@@ -42,12 +54,15 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {error && <Error>{error}</Error>}
+
       <Repositories>
         {repositories.map(repo => (
-          <a key={repo.fullName} href="https://github.com/">
-            <img src={repo.owner.avatarUrl} alt={repo.owner.login} />
+          <a key={repo.full_name} href="https://github.com/">
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
             <div>
-              <strong>{repo.fullName}</strong>
+              <strong>{repo.full_name}</strong>
               <p>{repo.description}</p>
             </div>
             <FiChevronRight size="20" />
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -22,6 +26,15 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border-top: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+    border-left: 2px solid #fff;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: red;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -98,4 +111,10 @@ export const Repositories = styled.div`
       color: #cbcbcc;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: red;
+  margin-top: 10px;
 `;
